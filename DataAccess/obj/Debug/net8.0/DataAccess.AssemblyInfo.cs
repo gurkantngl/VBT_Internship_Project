5ad012377a22e275d396c4e1632257d808b0fb7b
Gürkan Töngel <92158772+gurkantngl@users.noreply.github.com>
2024-08-02 11:58:23 +0300

    JWT added

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4b2ccfcf3d319c2483c1c0fc544dfe6f24ab84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e44eab27c5a751760f696530ce54fd739655108")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
